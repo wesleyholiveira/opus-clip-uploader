2dd04b2e67ac8a94d47fcb696d026a452ba81e72
Wesley Oliveira <wesleyholiveir@gmail.com>
2026-04-30 18:58:20 -0300

    fix: changed obs sdk version to 30.1

--- .github/scripts/Build-Windows.ps1
+++ .github/scripts/Build-Windows.ps1
@@ -2,27 +2,28 @@
 param(
     [ValidateSet('x64')]
     [string] $Target = 'x64',
+
     [ValidateSet('Debug', 'RelWithDebInfo', 'Release', 'MinSizeRel')]
     [string] $Configuration = 'RelWithDebInfo'
 )
 
 $ErrorActionPreference = 'Stop'
 
-if ( $DebugPreference -eq 'Continue' ) {
+if ($DebugPreference -eq 'Continue') {
     $VerbosePreference = 'Continue'
     $InformationPreference = 'Continue'
 }
 
-if ( $env:CI -eq $null ) {
+if ($env:CI -eq $null) {
     throw "Build-Windows.ps1 requires CI environment"
 }
 
-if ( ! ( [System.Environment]::Is64BitOperatingSystem ) ) {
+if (-not [System.Environment]::Is64BitOperatingSystem) {
     throw "A 64-bit system is required to build the project."
 }
 
-if ( $PSVersionTable.PSVersion -lt '7.2.0' ) {
-    Write-Warning 'The obs-studio PowerShell build script requires PowerShell Core 7. Install or upgrade your PowerShell version: https://aka.ms/pscore6'
+if ($PSVersionTable.PSVersion -lt '7.2.0') {
+    Write-Warning 'The obs-studio PowerShell build script requires PowerShell Core 7.'
     exit 2
 }
 
@@ -34,12 +35,11 @@ function Build {
         exit 2
     }
 
-    $ScriptHome = $PSScriptRoot
-    $ProjectRoot = Resolve-Path -Path "$PSScriptRoot/../.."
+    $ProjectRoot = (Resolve-Path -Path "$PSScriptRoot/../..").Path
 
-    $UtilityFunctions = Get-ChildItem -Path $PSScriptRoot/utils.pwsh/*.ps1 -Recurse
+    $UtilityFunctions = Get-ChildItem -Path "$PSScriptRoot/utils.pwsh/*.ps1" -Recurse
 
-    foreach($Utility in $UtilityFunctions) {
+    foreach ($Utility in $UtilityFunctions) {
         Write-Debug "Loading $($Utility.FullName)"
         . $Utility.FullName
     }
@@ -47,28 +47,29 @@ function Build {
     Push-Location -Stack BuildTemp
     Ensure-Location $ProjectRoot
 
-    $CmakeArgs = @('--preset', "windows-ci-${Target}")
-    $CmakeBuildArgs = @('--build')
-    $CmakeInstallArgs = @()
+    $CmakeArgs = @(
+        '--preset', "windows-ci-${Target}"
+    )
 
-    if ( $DebugPreference -eq 'Continue' ) {
-        $CmakeArgs += ('--debug-output')
-        $CmakeBuildArgs += ('--verbose')
-        $CmakeInstallArgs += ('--verbose')
-    }
-
-    $CmakeBuildArgs += @(
-        '--preset', "windows-${Target}"
+    $CmakeBuildArgs = @(
+        '--build'
+        '--preset', "windows-ci-${Target}"
         '--config', $Configuration
         '--parallel'
         '--', '/consoleLoggerParameters:Summary', '/noLogo'
     )
 
-    $CmakeInstallArgs += @(
-        '--install', "build_${Target}"
-        '--prefix', "${ProjectRoot}/release/${Configuration}"
+    $CmakeInstallArgs = @(
+        '--install', "$ProjectRoot/build_${Target}"
+        '--prefix', "$ProjectRoot/release/${Configuration}"
         '--config', $Configuration
     )
+
+    if ($DebugPreference -eq 'Continue') {
+        $CmakeArgs += '--debug-output'
+        $CmakeBuildArgs += '--verbose'
+        $CmakeInstallArgs += '--verbose'
+    }
 
     Log-Group "Configuring ${ProductName}..."
     Invoke-External cmake @CmakeArgs
