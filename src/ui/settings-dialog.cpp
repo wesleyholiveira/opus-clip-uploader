d48d500560c471138bd36fe3ccaf7b0811970206
Wesley Oliveira <wesleyholiveir@gmail.com>
2026-06-17 01:07:33 -0300

    format: clang formatting

--- src/ui/settings-dialog.cpp
+++ src/ui/settings-dialog.cpp
@@ -182,7 +182,8 @@ void open_settings_impl(void *private_data)
 
 	QObject::connect(sourceLangInput, &QComboBox::currentTextChanged, &dialog,
 			 [gptDefaultPromptInput](const QString &sourceLanguage) {
-				 gptDefaultPromptInput->setPlainText(GptPromptClient::configuredInputTextTemplate(sourceLanguage));
+				 gptDefaultPromptInput->setPlainText(
+					 GptPromptClient::configuredInputTextTemplate(sourceLanguage));
 			 });
 
 	treeWidget->resizeColumnToContents(0);
